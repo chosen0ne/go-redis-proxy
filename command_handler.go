59fa04e34552d2eb3d7e70ae8ec582ad5d19ca18
chosen0ne <louzhenlin86@126.com>
2017-11-22 18:04:34 +0800

    - Update ARITY_DFT: -1 => 0 - Add comments

--- command_handler.go
+++ command_handler.go
@@ -10,11 +10,18 @@ const (
 	ARITY_MAX  = 1000    // maximum number of the command arugement
 	ARITY_EVEN = 1000000 // arguments number of the commend is even
 	ARITY_ODD  = 1000001 // arguments number of the commend is odd
-	ARITY_DFT  = -1      // no limit for argument number of the command
+	ARITY_DFT  = 0       // no limit for argument number of the command
 )
 
 type CommandHandler interface {
 	Name() string
+	// Arity() returns the rule of the arguments for the command.
+	// The value for Arity() may be as follows:
+	//	ARITY_DFT: no limitation
+	//	ARITY_EVEN: arguments count of the command must be even
+	//	ARITY_ODD: arguments count of the command must be odd
+	//  a positive number: arguments count of the command must equal the value
+	//	a negative number: arguments count of the command must not exceed the value
 	Arity() int
 	Handle(cmd *Command) (Response, error)
 }
